5c0331c58c8dd6f6e876d706e55d1e35af533203
EvgeniiYavorovenko <61652996+EvgeniiYavorovenko@users.noreply.github.com>
2020-05-21 18:18:01 +0300

    Add files via upload

---
+++ AzureAndAnsible/WebApplication2/WebApplication2/Controllers/GetKeyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Services;
+
+namespace WebApplication2.Controllers
+{
+    public class GetKeyController : PageModel
+    {
+        private IGetKeyService _getKeyService;
+        public GetKeyController(IGetKeyService getKeyService)
+        {
+            _getKeyService = getKeyService;
+        }
+
+        public string GetMyFullName()
+        {
+            var response = _getKeyService.GetFullName().myFullName;
+            return response;
+        }
+
+        public string GetConnectionString()
+        {
+            var response = _getKeyService.GetConnectionString().connectionString;
+            return response;
+        }
+    }
+}
---
+++ AzureAndAnsible/WebApplication2/WebApplication2/Services/GetKeyService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication2.Services
+{
+    public interface IGetKeyService
+    {
+        MyFullName GetFullName();
+        ConnectionString GetConnectionString();
+    }
+
+    public class GetKeyService : IGetKeyService
+    {
+        private IConfiguration _configuration;
+        public GetKeyService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public MyFullName GetFullName()
+        {
+            var response = new MyFullName
+            {
+                myFullName = _configuration.GetSection("MyFullName").Value
+            };
+            return response;
+        }
+
+        public ConnectionString GetConnectionString()
+        {
+            var response = new ConnectionString
+            {
+                connectionString = _configuration.GetConnectionString("DB")
+            };
+            return response;
+        }
+    }
+}
